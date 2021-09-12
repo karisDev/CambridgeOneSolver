b3b01659ba98854b2e61b30b453d5b984c507f87
karisDev <karjaakirill@gmail.com>
2021-09-12 21:43:29 +0300

    Add project files.

---
+++ CambridgeOneSolver/ViewModels/CambridgeWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CambridgeOneSolver.ViewModels.Base;
+
+namespace CambridgeOneSolver.ViewModels
+{
+    class CambridgeWindowViewModel : ViewModel
+    {
+        private string _Title = "Cambridge One Solver";
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
