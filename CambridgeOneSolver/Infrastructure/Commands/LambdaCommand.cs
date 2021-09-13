24ca86168e08620623c1eb1e8efe33115ed3c441
karisDev <karjaakirill@gmail.com>
2021-09-13 07:31:51 +0300

    Начало разработки ChromeDriver

--- CambridgeOneSolver/Infrastructure/Commands/LambdaCommand.cs
+++ CambridgeOneSolver/Infrastructure/Commands/LambdaCommand.cs
@@ -1,9 +1,5 @@
 ﻿using CambridgeOneSolver.Infrastructure.Commands.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CambridgeOneSolver.Infrastructure.Commands
 {
@@ -21,5 +17,7 @@ namespace CambridgeOneSolver.Infrastructure.Commands
         public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
 
         public override void Execute(object parameter) => _Execute(parameter);
+
+        
     }
 }
---
+++ CambridgeOneSolver/Models/Driver.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CambridgeOneSolver.Models
+{
+    internal class Driver
+    {
+        public static ChromeDriver driver;
+        public static void Start()
+        {
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+
+            driver = new ChromeDriver(driverService, new ChromeOptions());
+        }
+    }
+}
